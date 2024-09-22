aba955f07da185b5216be52126075f484d13bfab
direwolf7 <166890279+eisbaer137@users.noreply.github.com>
2024-09-23 02:39:38 +0900

    Add files via upload
    A Chatting program using TCP/IP running on Linux(tested on WSL(4.4.0-19041 Microsoft))

---
+++ chatServer.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <iostream>
+#include <pthread.h>
+#include <set>
+#include "netSocket.h"
+
+#define BUFFER_SIZE	128
+//#define MAX_CLIENTS	256
+
+using namespace std;
+
+void* clientProcessing(void* arg);
+void sendMessage(char* message, int len);
+
+int client_count = 0;
+//int clnt_socks[MAX_CLIENTS];
+set<int> client_socks;
+pthread_mutex_t mTex;
+
+int main(int argc, char** argv)
+{
+	int port;
+
+	std::cout << "Input port number : ";
+	std::cin >> port;
+
+	NetSocket servSock(" ", port, 5);
+	pthread_t t_id;
+
+	pthread_mutex_init(&mTex, NULL);	// initialize a mutex object
+	servSock.InitializeSocket(TCP);
+	servSock.SetServerAddress(SERVER);
+	servSock.BindAddressToSocket();
+	
+	servSock.WaitConnectionRequest();
+
+	while(true)
+	{
+		servSock.AcceptIncoming();
+
+		pthread_mutex_lock(&mTex);
+		//client_socks[client_count++] = servSock.GetClientSocket();
+		client_socks.insert(servSock.GetClientSocket());
+		client_count = client_socks.size();
+		
+		pthread_mutex_unlock(&mTex);
+
+		pthread_create(&t_id, NULL, clientProcessing, (void*)servSock.GetClientSocketP());
+		pthread_detach(t_id);
+		printf("Connected client IP : %s \n", inet_ntoa(servSock.GetClientAddress().sin_addr));
+	}
+
+	return 0;
+}
+
+void* clientProcessing(void* arg)
+{
+	int client_sock = *((int*)arg);
+	int str_len = 0;
+	int i;
+	char msg[BUFFER_SIZE];
+
+	while((str_len = read(client_sock, msg, sizeof(msg))) != 0)
+	{
+		sendMessage(msg, str_len);
+	}
+
+	pthread_mutex_lock(&mTex);
+	auto iter = client_socks.find(client_sock);
+	client_socks.erase(iter);
+	client_count = client_socks.size();
+	pthread_mutex_unlock(&mTex);
+
+	return NULL;
+}
+
+void sendMessage(char* message, int len)
+{
+	int i;
+	pthread_mutex_lock(&mTex);
+	for(auto iter = client_socks.begin(); iter != client_socks.end(); ++iter)
+	{
+		write(*iter, message, len);
+	}
+	pthread_mutex_unlock(&mTex);
+}
---
+++ netSocket.h
@@ -0,0 +1,164 @@
+#ifndef __NET_SOCKET_H__
+
+#define __NET_SOCKET_H__
+
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define TCP     0  
+#define UDP     1
+
+#define SERVER  0
+#define CLIENT  1
+
+void ErrorHandling(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+class NetSocket
+{
+public:
+    NetSocket();
+    NetSocket(std::string _ip, int _pt, int _backlog);
+    ~NetSocket();
+    NetSocket(const NetSocket& rhs) = delete;
+    NetSocket& operator=(const NetSocket& rhs) = delete;
+
+public:
+    void InitializeSocket(int flag);
+    void SetServerAddress(int flag);
+    void BindAddressToSocket();
+    void WaitConnectionRequest();
+    void AcceptIncoming();
+    void ConnectToServer();
+
+    int GetServerSocket();
+    int GetClientSocket();
+    int* GetClientSocketP();
+    struct sockaddr_in  GetClientAddress();
+
+private:
+    int mServer_sock;
+    int mClient_sock;
+    struct sockaddr_in mServer_addr;
+    struct sockaddr_in mClient_addr;
+    int mClient_addr_sz;
+
+    std::string mIP;
+    int mPort;
+    int mBackLogQueue_sz;       // size of the backlog queue 
+
+};
+
+
+NetSocket::NetSocket()
+{
+
+}
+
+NetSocket::NetSocket(std::string _ip, int _pt, int _backlog)
+{
+    mIP = _ip;
+    mPort = _pt;
+    mBackLogQueue_sz = _backlog;
+}
+
+NetSocket::~NetSocket()
+{
+    if(mServer_sock > 2)
+    {
+        close(mServer_sock);
+    }
+    if(mClient_sock > 2)
+    {
+        close(mClient_sock);
+    }
+}
+
+void NetSocket::InitializeSocket(int flag)
+{
+    if(flag == TCP)
+    {
+        mServer_sock = socket(PF_INET, SOCK_STREAM, 0);
+    }
+    else if(flag == UDP)
+    {
+        mServer_sock = socket(PF_INET, SOCK_DGRAM, 0);
+    }
+}
+
+void NetSocket::SetServerAddress(int flag)
+{
+    memset(&mServer_addr, 0, sizeof(mServer_addr));
+    mServer_addr.sin_family = AF_INET;
+    if(flag == SERVER)
+    {
+        mServer_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    }
+    else if(flag == CLIENT)
+    {
+        mServer_addr.sin_addr.s_addr = inet_addr(mIP.c_str());
+    }
+    mServer_addr.sin_port = htons(mPort);
+}
+
+void NetSocket::BindAddressToSocket()
+{
+    if(bind(mServer_sock, (struct sockaddr*)&mServer_addr, sizeof(mServer_addr))==-1)
+    {
+        ErrorHandling("bind(...) failed. This program is terminated...");
+    }
+}
+
+void NetSocket::WaitConnectionRequest()
+{
+    if(listen(mServer_sock, mBackLogQueue_sz) == -1)
+    {
+        ErrorHandling("listen(...) failed. This program is terminated...");
+    }
+}
+
+void NetSocket::AcceptIncoming()
+{
+    mClient_addr_sz = sizeof(mClient_addr);
+    mClient_sock = accept(mServer_sock, (struct sockaddr*)&mClient_addr, (socklen_t*)&mClient_addr_sz);
+}
+
+void NetSocket::ConnectToServer()
+{
+    if(connect(mServer_sock, (struct sockaddr*)&mServer_addr, sizeof(mServer_addr)) == -1)
+    {
+        ErrorHandling("connect(...) failed. This program is terminated...");
+    }
+}
+
+int NetSocket::GetServerSocket()
+{
+    return mServer_sock;
+}
+
+int NetSocket::GetClientSocket()
+{
+    return mClient_sock;
+}
+
+int* NetSocket::GetClientSocketP()
+{
+    return &mClient_sock;
+}
+
+struct sockaddr_in NetSocket::GetClientAddress()
+{
+    return mClient_addr;
+}
+
+
+#endif
